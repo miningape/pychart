0a6262d4bc123a12dd4cbf3a132040cfa92bea10
Kyle <kyle@weld.app>
2022-02-16 00:13:11 +0100

    install setup

---
+++ main_test.py
@@ -0,0 +1,8 @@
+# Will need to merge or do some shit to get main fixed
+
+def inc(x: int) -> int:
+  return x + 1
+
+
+def test_answer():
+  assert inc(1) == 3
---
+++ scripts/freeze.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 -m pip freeze > requirements.txt
