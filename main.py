7fe127824feae6b66142e8842017a83fc14eba1e
Kyle <kyle@weld.app>
2022-02-17 18:06:56 +0100

    linting

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from scanner import Scanner
 
 if __name__ == "__main__":
-  program = "1 + (2 / 3)"
+    program = "1 + (2 / 3)"
 
-  scanner = Scanner(program)
-  tokens = scanner.getTokens()
+    scanner = Scanner(program)
+    tokens = scanner.getTokens()
 
-  print(tokens)
+    print(tokens)
