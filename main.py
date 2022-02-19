19c1c39df5adc24ce16b19771a83b549d10b609f
Kyle <kyle@weld.app>
2022-02-19 02:24:54 +0100

    wip

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ if __name__ == "__main__":
     program = "(){}+-*"  # "1 + (2 / 3)"
     scanner = Scanner(program)
     tokens = scanner.get_tokens()
-    for token in tokens:
-        print(token)
+    print(list(map(lambda tok: str(tok), tokens)))
