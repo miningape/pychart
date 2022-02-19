9bba43869e1856228f348361d9b2a2b6140cdaf3
Kyle <kyle@weld.app>
2022-02-19 01:35:33 +0100

    fixed scanner

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     program = "1 + (2 / 3)"
 
     scanner = Scanner(program)
-    tokens = scanner.getTokens()
+    tokens = scanner.get_tokens()
 
     print(tokens)
--- scanner.py
+++ scanner.py
@@ -1,50 +1,50 @@
 from typing import List
-from Token.exporter import Token, TokenType
+from token.exporter import Token, TokenType
 
 
 class Scanner:
-  # Source variables
-  source: str = ""
-  tokens: List[Token] = []
+    # Source variables
+    source: str = ""
+    tokens: List[Token] = []
 
-  # Variables to track scanner
-  start: int = 0
-  current: int = 0
-  line: int = 1
+    # Variables to track scanner
+    start: int = 0
+    current: int = 0
+    line: int = 1
 
-  def __init__(self, programText: str):
-    self.source = programText
+    def __init__(self, programText: str):
+        self.source = programText
 
-  def __isAtEnd(self) -> bool:
-    return self.current >= len(self.source)
+    def __is_at_end(self) -> bool:
+        return self.current >= len(self.source)
 
-  def __advance(self) -> str:
-    return ""
+    def __advance(self) -> str:
+        return ""
 
-  def __add_token(self, type: TokenType):
-    pass
+    def __add_token(self, type: TokenType):
+        pass
 
-  def __scan_token(self):
-    char = self.__advance()
+    def __scan_token(self):
+        char = self.__advance()
 
-    if char == "+":
-      self.__add_token(TokenType.Plus)
-    elif char == "-":
-      self.__add_token(TokenType.Minus)
-    elif char == "/":
-      self.__add_token(TokenType.Slash)
-    elif char == "*":
-      self.__add_token(TokenType.Star)
+        if char == "+":
+            self.__add_token(TokenType.PLUS)
+        elif char == "-":
+            self.__add_token(TokenType.MINUS)
+        elif char == "/":
+            self.__add_token(TokenType.SLASH)
+        elif char == "*":
+            self.__add_token(TokenType.STAR)
 
-  def getTokens(self) -> List[Token]:
-    if len(self.tokens) != 0:
-      return self.tokens
+    def get_tokens(self) -> List[Token]:
+        if len(self.tokens) != 0:
+            return self.tokens
 
-    # do the tokenising
-    while not self.__isAtEnd():
-      self.start = self.current
-      self.__scan_token()
+      # do the tokenising
+        while not self.__is_at_end():
+            self.start = self.current
+            self.__scan_token()
 
-    self.tokens.append(Token(TokenType.EOF, "", None, self.line))
+        self.tokens.append(Token(TokenType.EOF, "", None, self.line))
 
-    return self.tokens
+        return self.tokens
