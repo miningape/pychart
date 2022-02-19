a4959abe70659a25f7d68c5b189d3d3e75c94317
Kyle <kyle@weld.app>
2022-02-19 04:27:08 +0100

    stupid linting

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from scanner import Scanner
 
 if __name__ == "__main__":
-    program = "(){}+-*"  # "1 + (2 / 3)"
+    program = f""  # "1 + (2 / 3)"
 
     scanner = Scanner(program)
     tokens = scanner.get_tokens()
 
-    print(list(map(lambda tok: str(tok), tokens)))
+    print(list(map(str, tokens)))
