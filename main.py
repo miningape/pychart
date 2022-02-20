f2675b130fd424610afb82de0eb585547474a909
Kyle <kyle@weld.app>
2022-02-20 01:42:09 +0100

    repl

---
+++ main.py
@@ -0,0 +1,35 @@
+from shutil import ExecError
+import sys
+from typing import List
+
+
+def run(source: str):
+    print(source)
+
+
+def runPrompt():
+    while True:
+        line = input("$ : ")
+
+        if line == ".exit":
+            break
+
+        run(line)
+
+
+def runFile(filename: str):
+    with open(filename, "r") as contents:
+        run(contents.read())
+
+
+def main(argc: int, argv: List[str]):
+    if argc > 2:
+        raise ExecError("Too many arguments")
+    elif argc == 2:
+        runFile(argv[1])
+    else:
+        runPrompt()
+
+
+if __name__ == "__main__":
+    main(len(sys.argv), sys.argv)
