376890957dda32a88c96f3c83bb1fd20fd5ee9b6
Robert Spralja <robert.spralja@gmail.com>
2022-02-23 21:08:52 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='pychart',
+    version='0.0.0',
+    description='',
+    author='',
+    author_email='',
+    url='https://github.com/miningape/pychart',
+    packages=['pychart'],
+)
