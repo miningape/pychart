21696a975d2d92177370622333573a904adfa764
Robert Spralja <56681655+spralja@users.noreply.github.com>
2022-02-26 12:41:18 +0100

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -2,10 +2,6 @@ import sys
 sys.path.append('src')
 
 import pychart
-import pychart.scanner
-import pychart.token_type
-import pychart.token_type.token_type_enum
-import pychart.token_type.token
 
 def inc(number: int) -> int:
     return number + 1
