390627164889255a986abe4c34838bf1c6900d84
Robert Spralja <robert.spralja@gmail.com>
2022-03-02 23:43:03 +0100

    added --version/-V argument to CLI

--- src/pychart/__main__.py
+++ src/pychart/__main__.py
@@ -1,12 +1,16 @@
+from . import __version__
 import argparse
 from .runner import run_prompt, run_file
 
 def main():
     parser = argparse.ArgumentParser(prog='pychart')
     parser.add_argument('file', nargs='?', help='run a pychart file')
+    parser.add_argument('--version', '-V', action='store_true')
 
     kwargs = vars(parser.parse_args())
-    if kwargs.get('file'):
+    if kwargs.pop('version'):
+        print(f'v{__version__}')
+    elif kwargs.get('file'):
         run_file(kwargs.pop('file'))
     else:
         run_prompt()
