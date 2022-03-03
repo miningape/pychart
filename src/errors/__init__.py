7783ab3239ae6e4349bdb44728bd6c9c15ef6ecf
Robert Spralja <robert.spralja@gmail.com>
2022-03-03 18:10:16 +0100

    fixed errors in SyntaxError.__str__

--- src/errors/__init__.py
+++ src/errors/__init__.py
@@ -21,22 +21,20 @@ class SyntaxError(BaseError):
         current_line = current_token.line
 
         start_of_line = self.current
-        while self.tokens[start_of_line].line == current_line:
+        while start_of_line > 0 and self.tokens[start_of_line].line == current_line:
             start_of_line -= 1
 
-        start_of_line += 1
-
         end_of_line = self.current
-        while self.tokens[end_of_line].line == current_line:
+        while end_of_line < len(self.tokens) and self.tokens[end_of_line].line == current_line:
             end_of_line += 1
 
 
-        for token in self.tokens[start_of_line:end_of_line]:
-            if token == current_token:
-                string += '~~'
-            string += token.lexem + ' '
+        for token, index in zip(self.tokens[start_of_line:end_of_line], range(start_of_line, end_of_line)):
+            if index == self.current:
+                string += '~~ '
+            string += token.lexeme + ' '
 
-            if token == current_token:
+            if index == self.current:
                 string += '~~'
 
         return string
